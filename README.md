d550c392ec31428694b5f9b896ee8fe99346fc09
Foad Lind <foad.lind@citynetwork.eu>
2022-02-28 15:16:07 +0100

    Create tutor-contrib-retirement plugin
    Structure created from Tutor plugin cookiecutter:
    https://github.com/overhangio/cookiecutter-tutor-plugin
    Changed the REDME to use Markedown and derive the
    version number from a Git tag with setuptools-scm.

---
+++ README.md
@@ -0,0 +1,17 @@
+User Retirement Tutor plugin
+===================================
+
+This is an **experimental** plugin for
+[Tutor](https://docs.tutor.overhang.io) that enables the [user retirement 
+feature](https://edx.readthedocs.io/projects/edx-installing-configuring-and-running/en/latest/configuration/user_retire/driver_setup.html) in Open edX.
+
+Installation
+------------
+
+    pip install git+https://github.com/hastexo/tutor-contrib-retirement
+
+Usage
+-----
+
+
+    tutor plugins enable retirement
---
+++ setup.py
@@ -0,0 +1,45 @@
+import io
+import os
+from setuptools import setup, find_packages
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def load_readme():
+    with io.open(os.path.join(HERE, "README.md"), "rt", encoding="utf8") as f:
+        return f.read()
+
+
+setup(
+    name="tutor-contrib-retirement",
+    use_scm_version=True,
+    url="https://github.com/hastexo/tutor-contrib-retirement",
+    project_urls={
+        "Code": "https://github.com/hastexo/tutor-contrib-retirement",
+        "Issue tracker": "https://github.com/hastexo/tutor-contrib-retirement/issues",
+    },
+    license="AGPLv3",
+    author="hastexo",
+    description="User retirement plugin for Tutor",
+    long_description=load_readme(),
+    long_description_content_type='text/markdown',
+    packages=find_packages(exclude=["tests*"]),
+    include_package_data=True,
+    python_requires=">=3.8",
+    install_requires=["tutor"],
+    setup_requires=["setuptools-scm"],
+    entry_points={
+        "tutor.plugin.v0": [
+            "retirement = tutorretirement.plugin"
+        ]
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
