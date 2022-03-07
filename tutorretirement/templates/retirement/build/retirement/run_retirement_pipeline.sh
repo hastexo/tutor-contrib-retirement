f1f1455226355b0f9277c9e52ad5224b676b2979
Foad Lind <foad.lind@citynetwork.eu>
2022-03-07 14:15:26 +0100

    fix: Add pipeline config file as ConfigMap
    Instead of directly including the pipeline config file in the image,
    mount it in a volume in docker compose and as ConfigMap in Kubernetes.

--- tutorretirement/templates/retirement/build/retirement/run_retirement_pipeline.sh
+++ tutorretirement/templates/retirement/build/retirement/run_retirement_pipeline.sh
@@ -1,5 +1,5 @@
 cool_off_days=$1
-python scripts/get_learners_to_retire.py --config_file=config.yml --output_dir=learners_to_retire --cool_off_days="$cool_off_days"
+python scripts/get_learners_to_retire.py --config_file=./pipeline_config/config.yml --output_dir=learners_to_retire --cool_off_days="$cool_off_days"
 for filename in learners_to_retire/*;do
     if test ! -f "$filename";then
         echo "No users to retire"
@@ -7,6 +7,6 @@ for filename in learners_to_retire/*;do
     else
         IFS="=" read -r name username <"$filename"
         echo "Retiring $username"
-        python scripts/retire_one_learner.py --config_file=config.yml --username="$username"
+        python scripts/retire_one_learner.py --config_file=./pipeline_config/config.yml --username="$username"
     fi
 done
